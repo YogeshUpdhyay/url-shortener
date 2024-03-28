32b7b4c1d2f9b7b9e680314d5a00d4ad8d8a50b1
Yogesh.Upadhyay <yogesh.upadhyay@24B-MACABL-0889.local>
2024-03-28 12:45:47 +0530

    Delete controllers implemented

--- README.md
+++ README.md
@@ -1,13 +1,10 @@
 # URL Shortener
 ---
+
 ### Development environment
+To run the server use air
 ```
+go mod download
 go install github.com/cosmtrek/air@latest
-air --build.cmd "go build -o bin/api main.go" --build.bin "./bin/api"
-```
-
-
-### Run command
-```
-go run main.go
+air
 ```
--- internal/controllers/deleteurl.go
+++ internal/controllers/deleteurl.go
@@ -5,9 +5,43 @@ import (
 
 	"net/http"
 	"github.com/gin-gonic/gin"
+
+	"url-shortner/internal/models"
+	"url-shortner/internal/initializers"
 )
 
+type DeleteUrlPayload struct {
+	Slug string `json:"slug"`
+}
+
 func DeleteUrl(c *gin.Context) {
-	log.Println("Deleten the url")
+	log.Println("Delete the url")
+
+	// parsing the payload
+	var payload DeleteUrlPayload
+	err := c.BindJSON(&payload)
+	if err != nil {
+		// Return a 400 Bad Request response if JSON binding fails
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	var shortUrl models.ShortUrl
+	result := initializers.DB.First(&shortUrl, "slug = ?", payload.Slug)
+	if result.Error != nil {
+		// Log the error and return a 500 Internal Server Error response
+		log.Println("Error deleting shorturl:", result.Error)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+
+	deleteResult := initializers.DB.Delete(&shortUrl)
+	if deleteResult.Error != nil {
+		// Log the error and return a 500 Internal Server Error response
+		log.Println("Error deleting shorturl:", deleteResult.Error)
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Internal server error"})
+		return
+	}
+
 	c.JSON(http.StatusOK, gin.H{"msg": "OK"})
 }
